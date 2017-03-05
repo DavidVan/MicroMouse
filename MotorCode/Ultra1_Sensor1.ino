4c846536a7d524ac018f6787ef5b9c5a6079e384
Elizabeth Nguyen <e.k.nguyen31@gmail.com>
2017-03-04 16:32:00 -0800

    Jan. 28, 2017

---
+++ MotorCode/Ultra1_Sensor1.ino
@@ -0,0 +1,100 @@
+#include <AccelStepper.h>
+#define HALFSTEP 8
+
+// initialize the stepper library on pins 8 through 11:
+#define motor1Pin1 4
+#define motor1Pin2 5
+#define motor1Pin3 6
+#define motor1Pin4 7
+#define ECHO1 2
+#define TRIG1 3
+
+#define motor2Pin1 10
+#define motor2Pin2 11
+#define motor2Pin3 12
+#define motor2Pin4 13
+#define ECHO2 8
+#define TRIG2 9
+
+//Initialize with pin sequence using AccelStepper
+AccelStepper stepper1(HALFSTEP, motor1Pin1, motor1Pin3, motor1Pin2, motor1Pin4);
+AccelStepper stepper2(HALFSTEP, motor2Pin1, motor2Pin3, motor2Pin2, motor2Pin4);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO2, INPUT);
+  pinMode(TRIG2, OUTPUT);
+  stepper1.setMaxSpeed(2000.0);
+  stepper1.setSpeed(1000);
+  stepper2.setMaxSpeed(2000.0);
+  stepper2.setSpeed(1000);
+}
+
+void loop()
+{
+  // Ultrasonic
+  long duration1, distance1, duration2, distance2;
+  digitalWrite(TRIG1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1,LOW);
+  duration1 = pulseIn(ECHO1, HIGH);
+  distance1 = duration1/29.1;
+
+  digitalWrite(TRIG2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG2, LOW);
+  duration2 = pulseIn(ECHO2, HIGH);
+  distance2 = duration2/29.1;
+ 
+// Sensor One  
+  if (distance1 > 100)
+  {
+  stepper1.runSpeed();
+  }
+  else if (distance1 <= 100 && distance1 > 60)
+  {
+  stepper1.setSpeed(500);
+  stepper1.runSpeed();
+  }
+  else if (distance1 <= 60 && distance1 > 20)
+  {
+  stepper1.setSpeed(250);
+  stepper1.runSpeed();
+  }
+  else if (distance1 <= 20)
+  {
+  stepper1.stop();
+  }
+
+// Sensor Two  
+  if (distance2 > 100)
+  {
+  stepper2.runSpeed();
+  }
+  else if (distance2 <= 100 && distance2 > 60)
+  {
+  stepper2.setSpeed(500);
+  stepper2.runSpeed();
+  }
+  else if (distance2 <= 60 && distance2 > 20)
+  {
+  stepper2.setSpeed(250);
+  stepper2.runSpeed();
+  }
+  else if (distance2 <= 20)
+  {
+  stepper2.stop();
+  }
+  Serial.print(distance1);
+  Serial.print(distance2);
+  Serial.print("\n");
+  stepper1.run();
+  stepper2.run();
+}
