a054f4d762f98dcb8208089ff70ef476e4b2df5a
Harold Agnote <haroldagnote38@hotmail.com>
2017-07-22 16:02:31 -0700

    Fixed Main.cpp

--- MicroMouse/Main.cpp
+++ MicroMouse/Main.cpp
@@ -1,25 +1,54 @@
-#include "Queue.h"
+#include <iostream>
+#include "Block.h"
+#include "Stack.h"
+#include "Maze.h"
+#include "Coord.h"
 
 using namespace std;
 
-int main()
-{
-	Queue <char> queue;
+int main() {
+	Block block;
+	cout << "Current: " << (int)block.GetBlock() << endl;
+	block.Set(Block::Bits::WestWall, true);
+	cout << "After Setting West Wall: " << (int)block.GetBlock() << endl;
+	block.Set(Block::Bits::EastWall, true);
+	cout << "After Setting East Wall: " << (int)block.GetBlock() << endl;
+	cout << "Check West Wall Status: " << block.Get(Block::Bits::WestWall) << endl;
+	block.Set(Block::Bits::WestWall, false);
+	cout << "After Unsetting West Wall: " << (int)block.GetBlock() << endl;
+	cout << "Check West Wall Status: " << block.Get(Block::Bits::WestWall) << endl;
+	cout << "Check East Wall Status: " << block.Get(Block::Bits::EastWall) << endl;
+	int x;
 
-	char x = '0';
+	Maze maze;
+	maze.Initialize();
+	maze.PrintDistance();
 
-	do
-	{
-		cin >> x;
-		queue.enqueue(x);
+	cout << endl << endl << endl;
 
-	} while (x != '0');
+	maze.FloodFill();
+	maze.PrintDistance();
 
-	while (!queue.isEmpty())
-	{
-		cout << queue.dequeue() << endl;
+	/*Stack<Coord*> stack;
+	cout << "Initialized?: " << stack.size() << endl;
+
+	for (int i = 0; i < 17; i++) {
+	cout << i << endl;
+	Coord *t = new Coord(i, i);
+	stack.push(t);
 	}
 
-	system("pause");
-	return 0;
+	cout << "Stack size: " << stack.size() << endl;
+
+	Coord *item = stack.pop();
+
+	cout << "Item: " << (int)(item->GetRow()) << endl;
+
+	delete item;
+
+	cout << "Item: " << (int)(item->GetRow()) << endl;
+
+	cout << "Stack size: " << stack.size() << endl;*/
+
+	cin >> x;
 }
